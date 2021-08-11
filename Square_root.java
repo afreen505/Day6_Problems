05aac59e1789ec78d78eb4dbdb5fd588cc1bfaa2
afreen505 <58416095+afreen505@users.noreply.github.com>
2021-08-12 01:16:56 +0530

    Day6_Problems

---
+++ Square_root.java
@@ -0,0 +1,27 @@
+public class Square_root {
+    public static void main(String[] args)
+    {
+
+        System.out.println("Enter number: ");
+        double c = Double.parseDouble(args[0]);
+        // int Y = Integer.parseInt(args[0]);
+
+
+        double t = c;
+
+        double epsilon = 1e-15;
+
+
+        while ( Math.abs(t - c / t) > epsilon * t)
+        {
+            t = ((c / t) + t) / 2;
+
+        }
+
+        System.out.println("Square Root is "+t);
+    }
+
+
+
+}
+
---
+++ Vending_Machine.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Vending_Machine {
+    public static void main(String[] args)
+    {
+        int note;
+        int [] VmachineNotes= {1,2,5,10,20,50,100,500,1000};
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the money");
+        double money=sc.nextInt();
+        int minnotes=0;
+        for(int i=7;i>=0;i--)
+        {
+            if(money>=VmachineNotes[i])
+            {
+                note=(int) (money/VmachineNotes[i]);
+                money=money-note*VmachineNotes[i];
+                minnotes=minnotes+note;
+                System.out.println("note:"+VmachineNotes[i]+" number or of notes "+note);
+            }
+        }
+        System.out.println("total number of notes "+minnotes);
+
+    }
+
+}
+
